abc0dd59a0e823f3962816e246b87a07462dd329
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-13 06:36:10 +0300

    Update sum_of_a_beach.py

--- basic_syntax_conditional_statements_and_loops/sum_of_a_beach.py
+++ basic_syntax_conditional_statements_and_loops/sum_of_a_beach.py
@@ -1,3 +1,5 @@
+# Beaches are filled with sand, water, fish, and sun. Given a string, calculate how many times the words "Sand", "Water", "Fish", and "Sun" appear (case insensitive).
+
 text = input()
 text = text.lower()
 
