2248ab37c3eb5c01c538f55186bb3de3b3b3bfd6
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-13 06:30:13 +0300

    Update can't_sleep_count_sheep.py

--- basic_syntax_conditional_statements_and_loops/can't_sleep_count_sheep.py
+++ basic_syntax_conditional_statements_and_loops/can't_sleep_count_sheep.py
@@ -1,3 +1,6 @@
+# If you can't fall asleep, try counting sheep! You will be given a positive integer, 3, for example. You should return a string with a murmur:
+# "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e., integers greater than 0.
+
 sheep_counter = int(input())
 
 for i in range(1,sheep_counter +1):
