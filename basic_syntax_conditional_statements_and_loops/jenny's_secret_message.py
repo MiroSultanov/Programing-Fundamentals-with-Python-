33288d3c74d7f64d84b934bdc2a93ca3cd929399
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-13 06:27:33 +0300

    Update jenny's_secret_message.py

--- basic_syntax_conditional_statements_and_loops/jenny's_secret_message.py
+++ basic_syntax_conditional_statements_and_loops/jenny's_secret_message.py
@@ -1,3 +1,6 @@
+# Jenny studies programming with Python and wants to create a program that greets users when they give their names. The greeting should be in the format 
+# "Hello, {name}!". However, Jenny is in love with Johnny and would like to greet him differently: "Hello, my love!". Could you help her?
+
 name = input()
 
 if name == "Johnny":
