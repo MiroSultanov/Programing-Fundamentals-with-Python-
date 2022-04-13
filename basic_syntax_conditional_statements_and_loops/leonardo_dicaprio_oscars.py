f117023c0385493fef758ec716d762a3590c5911
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-13 06:28:51 +0300

    Update leonardo_dicaprio_oscars.py

--- basic_syntax_conditional_statements_and_loops/leonardo_dicaprio_oscars.py
+++ basic_syntax_conditional_statements_and_loops/leonardo_dicaprio_oscars.py
@@ -1,3 +1,10 @@
+# Write a program that receives a single integer number and prints different messages depending on the number:
+# If the number is 88 - "Leo finally won the Oscar! Leo is happy".
+# If the number is 86 - "Not even for Wolf of Wall Street?!"
+# If the number is not 88 nor 86 (and below 88) - "When will you give Leo an Oscar?"
+# If the number is over 88 - "Leo got one already!"
+
+
 number = int(input())
 
 if number == 88:
