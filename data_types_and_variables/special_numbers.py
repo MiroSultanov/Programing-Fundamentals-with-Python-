d9ca7a3195f969d6bc1ccd4756417e2deff8b085
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-15 23:46:02 +0300

    Update special_numbers.py

--- data_types_and_variables/special_numbers.py
+++ data_types_and_variables/special_numbers.py
@@ -1,3 +1,6 @@
+# Write a program that reads an integer n. Then, for all numbers in the range [1, n], prints the number and if it is special or not (True / False). 
+# A number is special when the sum of its digits is 5, 7, or 11.
+
 number = int(input())
 
 for num in range(1, number + 1):
