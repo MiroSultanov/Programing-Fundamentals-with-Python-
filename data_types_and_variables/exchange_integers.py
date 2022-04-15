3df76b02f8d4541843be853811ac3bc95e76bf1f
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-15 23:54:12 +0300

    Update exchange_integers.py

--- data_types_and_variables/exchange_integers.py
+++ data_types_and_variables/exchange_integers.py
@@ -1,3 +1,5 @@
+# Read two integer numbers and, after that, exchange their values. Print the variable values before and after the exchange, as shown below:
+
 a = int(input())
 b = int(input())
 new_a = 0
