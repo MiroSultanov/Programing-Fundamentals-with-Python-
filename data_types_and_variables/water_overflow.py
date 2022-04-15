7174a1e871edd4c68d2890ac816ceedacd550f64
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-15 23:51:56 +0300

    Update water_overflow.py

--- data_types_and_variables/water_overflow.py
+++ data_types_and_variables/water_overflow.py
@@ -1,3 +1,7 @@
+# You have a water tank with a capacity of 255 liters. On the first line, you will receive n – the number of lines, which will follow. 
+# On the following n lines, you will receive liters of water (integers), which you should pour into your tank. If the capacity is not enough, print "Insufficient capacity!" 
+# and continue reading the next line. On the last line, print the liters in the tank.
+
 n = int(input())
 water_tank = 255
 capacity = 0
