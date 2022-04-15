947ec0ca5c7d9baefb89403076e419b1c89a7951
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-15 23:50:50 +0300

    Update print_part_of_the_ASCII_table.py

--- data_types_and_variables/print_part_of_the_ASCII_table.py
+++ data_types_and_variables/print_part_of_the_ASCII_table.py
@@ -1,3 +1,7 @@
+# Write a program that prints part of the ASCII table characters on the console, separated by a single space. 
+# On the first line of input, you will receive the char index you should start with.
+# On the second line - the index of the last character you should print. 
+
 begin_char = int(input())
 end_char = int(input())
 
