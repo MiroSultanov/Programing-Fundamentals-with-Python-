476ed267beb5ae893bfbe5d735be30b97ab8bf38
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-17 10:06:30 +0300

    Update Courses.py

--- lists_basics/Courses.py
+++ lists_basics/Courses.py
@@ -1,3 +1,5 @@
+# On the first line, you will receive a single number n. On the following n lines, you will receive names of courses. You should create a list of courses and print it.
+
 number = int(input())
 courses = []
 
