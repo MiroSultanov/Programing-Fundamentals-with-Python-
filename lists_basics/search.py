ae050b9f0412d919fb06552d4cead562cc69223e
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-17 10:07:41 +0300

    Update search.py

--- lists_basics/search.py
+++ lists_basics/search.py
@@ -1,3 +1,6 @@
+# On the first line, you will receive a number n. On the second line, you will receive a word. On the following n lines, you will be given some strings. 
+# You should add them to a list and print them. After that, you should filter out only the strings that include the given word and print that list too.
+
 numbers = int(input())
 search_word = input()
 filtered = list()
