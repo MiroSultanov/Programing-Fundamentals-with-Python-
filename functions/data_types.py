fdb956dcb5e54b9a3a6935051c0bc79e76944ed9
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-18 06:35:58 +0300

    Update data_types.py

--- functions/data_types.py
+++ functions/data_types.py
@@ -1,14 +1,9 @@
-# def data_type_func():
-#     if data_type == "int":
-#         print(number * 2)
-#     elif data_type == "real":
-#         print(f"{number * 1.5:.2f}")
-#     elif data_type == 'string':
-#         print()
-#
-# data_type = input()
-# number = int(input())
-# data_type_func()
+# Write a function that, depending on the first line of the input, reads one of the following strings: "int", "real", or "string".
+# •	If the data type is an int, multiply the number by 2.
+# •	If the data type is real, multiply the number by 1.5 and format the result to the second decimal point.
+# •	If the data type is a string, surround the input with "$".
+# Print the result on the console.
+
 
 def data_type_func():
     if data_type == 'int':
