afd731f2473590dd1e42863cde632c006e60175c
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-18 06:30:03 +0300

    Update smallest_of_three_numbers.py

--- functions/smallest_of_three_numbers.py
+++ functions/smallest_of_three_numbers.py
@@ -1,3 +1,5 @@
+# Write a function that receives three integer numbers and returns the smallest. Print the result on the console. Use an appropriate name for the function
+
 def smallest_number(a, b, c):
     if a < b and a < c:
         return a
