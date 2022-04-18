0a6b556e7993226f0bbf61da1ce6f793412124d1
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-18 06:27:32 +0300

    Update calculations.py

--- functions/calculations.py
+++ functions/calculations.py
@@ -1,3 +1,7 @@
+# Create a function that receives three parameters, calculates a result depending on the given operator, and returns it. Print the result of the function.
+# The input comes as three parameters – an operator as a string and two integer numbers. The operator can be one of the following:  "multiply", "divide", "add", "subtract". 
+
+
 def calc_data(a, b, operator):
     if operator == 'multiply':
         return a * b
