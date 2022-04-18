f7b425d333477a0a76b20306ae8cdfd25cb872f1
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-18 06:37:47 +0300

    Update tribonacci-sequence.py

--- functions/tribonacci-sequence.py
+++ functions/tribonacci-sequence.py
@@ -1,3 +1,8 @@
+# In the Tribonacci sequence, every number is formed by the sum of the previous 3.
+# Write a function that prints numbers from the Tribonacci sequence on one line separated by a single space, starting from 1. 
+# You will receive a positive integer number as input.
+
+
 def tribonachi(x):
     num_list = [0,0,1]
     for _ in range(x - 1):
