2abc9046389abbe78168a411599773aee71a6335
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-18 06:32:06 +0300

    Update even_numbers.py

--- functions/even_numbers.py
+++ functions/even_numbers.py
@@ -1,2 +1,4 @@
+# Write a program that receives a sequence of numbers (integers) separated by a single space. It should print a list of only the even numbers. Use filter().
+
 result = list(filter(lambda x: x % 2 == 0, list(map(int, input().split(' ')))))
 print(result)
