9bc198477712a2082b201b345204f6e1ee38a1c6
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-18 06:32:28 +0300

    Update sort.py

--- functions/sort.py
+++ functions/sort.py
@@ -1,3 +1,5 @@
+# Write a program that receives a sequence of numbers (integers) separated by a single space. It should print a sorted list of numbers in ascending order. Use sorted().
+
 def sort_func(nums):
     return sorted(nums)
 numbers = list(map(int, input().split(' ')))
