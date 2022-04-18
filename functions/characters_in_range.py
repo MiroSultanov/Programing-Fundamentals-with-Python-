59fe286910bcd852f90bb27c06d93624e77fa964
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-18 06:31:02 +0300

    Update characters_in_range.py

--- functions/characters_in_range.py
+++ functions/characters_in_range.py
@@ -1,3 +1,6 @@
+# Write a function that receives two characters and returns a single string with all the characters in between them (according to the ASCII code), 
+# separated by a single space. Print the result on the console.
+
 def char_in_range(char_1, char_2):
     for i in range(ord(char_1) + 1, ord(char_2)):
         print(chr(i), end=' ')
