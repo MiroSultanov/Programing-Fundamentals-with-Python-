35983aa4c1a75bff67f375cf961581583690b4c0
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-18 06:34:18 +0300

    Update perfect_number.py

--- functions/perfect_number.py
+++ functions/perfect_number.py
@@ -1,3 +1,11 @@
+# A perfect number is a positive integer that is equal to the sum of its proper positive divisors. That is the sum of its positive divisors, excluding the number
+# itself (also known as its aliquot sum).
+# Write a function that receives an integer number and returns one of the following messages:
+# •	"We have a perfect number!" - if the number is perfect.
+# •	"It's not so perfect." - if the number is NOT perfect.
+# Print the result on the console.
+
+
 def perfect_num_func(number):
     if number > 0:
         num_sum = 0
