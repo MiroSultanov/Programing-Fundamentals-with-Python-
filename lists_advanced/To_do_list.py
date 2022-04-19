ce601991af311a4136c61590f4e041fdcb9673a0
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-19 06:19:53 +0300

    Update To_do_list.py

--- lists_advanced/To_do_list.py
+++ lists_advanced/To_do_list.py
@@ -1,3 +1,8 @@
+# You will be receiving to-do notes until you get the command "End". The notes will be in the format: "{importance}-{note}". 
+# Return a list containing all to-do notes sorted by importance in ascending order. 
+# The importance value will always be an integer between 1 and 10 (inclusive).
+
+
 todo = ["" for i in range(11)]
 command = input()
 
