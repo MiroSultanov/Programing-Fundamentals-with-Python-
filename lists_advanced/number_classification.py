5405c950c28df3eafb68c50256291efb8f2b030e
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-19 06:13:40 +0300

    Update number_classification.py

--- lists_advanced/number_classification.py
+++ lists_advanced/number_classification.py
@@ -1,3 +1,6 @@
+# Using a list comprehension, write a program that receives numbers, separated by comma and space ", ", and prints all the positive, negative, even, 
+# and odd numbers on separate lines as shown below.
+
 data = list(map(int, input().split(', ')))
 positive = [str(num) for num in data if num >= 0]
 negative = [str(num) for num in data if num < 0]
