d8c3981ac9030db2c5eac055a3515bbd73134b0b
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-19 06:11:59 +0300

    Update which_ are_in.py

--- lists_advanced/which_ are_in.py
+++ lists_advanced/which_ are_in.py
@@ -1,3 +1,6 @@
+# You will be given two sequences of strings, separated by ", ". 
+# Print a new list containing only the strings from the first input line, which are substrings of any string in the second input line.
+
 strings = input().split(', ')
 sentence = input()
 result = [el for el in strings if el in sentence]
