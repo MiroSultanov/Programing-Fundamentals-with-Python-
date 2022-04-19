8e6287b48696eec7b0136bc7c7547de2429cd35e
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-19 06:19:00 +0300

    Update no_vowels.py

--- lists_advanced/no_vowels.py
+++ lists_advanced/no_vowels.py
@@ -1,3 +1,6 @@
+# Using comprehension, write a program that receives a text and removes all its vowels, case insensitive. Print the new text string after removing the vowels. 
+# The vowels that should be considered are 'a', 'o', 'u', 'e', 'i'.
+
 vowels = ["a", "o", "u", "e", "i"]
 text = input()
 no_vowels_text = (ch for ch in text if ch not in vowels)
