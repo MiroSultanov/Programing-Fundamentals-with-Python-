dad84bce7c663230bee6a87a14f4aa6d79f2d27a
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-20 06:15:07 +0300

    Update inventory.py

--- objects_and_classes/inventory.py
+++ objects_and_classes/inventory.py
@@ -1,3 +1,12 @@
+# Create a class Inventory. The __init__ method should accept only the __capacity: int (private attribute) of the inventory. 
+#     You can read more about private attributes here. Each inventory should also have an attribute called items - empty list, where all the items will be stored. 
+#     The class should also have 3 methods:
+# •	add_item(item: str) - adds the item in the inventory if there is space for it. Otherwise, returns 
+# "not enough room in the inventory"
+# •	get_capacity() - returns the value of __capacity
+# •	__repr__() - returns "Items: {items}.\nCapacity left: {left_capacity}". The items should be separated by ", "
+
+
 class Inventory:
 
     def __init__(self, capacity: int):
