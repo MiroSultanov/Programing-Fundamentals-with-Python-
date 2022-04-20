815621ce768049298549d87fd273b465873509f3
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-20 06:15:32 +0300

    Update articles.py

--- objects_and_classes/articles.py
+++ objects_and_classes/articles.py
@@ -1,3 +1,10 @@
+# Create a class called Article. The __init__ method should accept 3 arguments: title: str, content: str, and author: str. The class should also have 4 methods:
+# •	edit(new_content: str) - changes the old content to the new one
+# •	change_author(new_author: str) - changes the old author with the new one
+# •	rename(new_title: str) - changes the old title with the new one
+# •	__repr__() - returns the following string "{title} - {content}: {author}"
+
+
 class Article:
     def __init__(self, title: str, content: str, author: str):
         self.title = title
