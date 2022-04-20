faaf49cf9a32dcb40e0edf3ebdffb307df5ea931
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-20 06:19:21 +0300

    Update circle.py

--- objects_and_classes/circle.py
+++ objects_and_classes/circle.py
@@ -1,3 +1,11 @@
+# Create a class Circle. In the __init__ method, the circle should only receive one parameter - its diameter. Create a class attribute called __pi that is equal to 3.14. 
+# The class should also have the following methods:
+# •	calculate_circumference() - returns the circumference of the circle
+# •	calculate_area() - returns the area of the circle
+# •	calculate_area_of_sector(angle) - gives the central angle in degrees, returns the area that fills the sector
+
+
+
 class Circle:
     __pi = 3.14
 
