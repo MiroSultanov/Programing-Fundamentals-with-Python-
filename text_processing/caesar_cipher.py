bc9b6ab473bff2966d6a3eba6cc575b42c34bcda
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-22 12:47:28 +0300

    Update caesar_cipher.py

--- text_processing/caesar_cipher.py
+++ text_processing/caesar_cipher.py
@@ -1,6 +1,5 @@
-# text = input()
-# output = "".join([chr(ord(x)+ 3) for x in text])
-# print(output)
+# Write a program that returns an encrypted version of the same text. Encrypt the text by replacing each character with the corresponding character three positions
+# forward in the ASCII table. For example, A would be replaced with D, B would become E, and so on. Print the encrypted text.
 
 def caesar_cipher(data):
     output = [chr(ord(ch) + 3) for ch in data]
