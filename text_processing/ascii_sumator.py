6d59f81acfaefa8dcef22eb95589fbd2637b11a9
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-22 12:56:28 +0300

    Update ascii_sumator.py

--- text_processing/ascii_sumator.py
+++ text_processing/ascii_sumator.py
@@ -1,3 +1,7 @@
+# Write a program that prints the sum of all found characters between two given characters (their ASCII code). On each of the first two lines, 
+# you will receive a single character. On the last line, you get a random string. Print the sum of the ASCII values of all characters in the random string between 
+# the two given characters in the ASCII table.
+
 def ascii_simulator():
     char_1 = ord(input())
     char_2 = ord(input())
