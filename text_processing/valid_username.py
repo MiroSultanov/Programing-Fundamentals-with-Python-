57bd8ffc6c07f43458f328a40fdfea75c1d31786
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-22 12:45:23 +0300

    Update valid_username.py

--- text_processing/valid_username.py
+++ text_processing/valid_username.py
@@ -1,15 +1,9 @@
-# import string
-#
-# valid_chars = [ch for ch in list(string.printable) if ch.isalnum() or ch in ("-","_")]
-#
-# lenght_chars = lambda x: True if 3 < len(x) <= 16 else False
-#
-# invalid_chars = lambda x: True if list(filter(lambda ch: ch not in valid_chars, x)) else False
-#
-# usernames = input().split(", ")
-# for names in usernames:
-#     if lenght_chars(names) and not invalid_chars(names):
-#         print(names)
+# Write a program that reads usernames on a single line (separated by ", ") and prints all valid usernames on separate lines. 
+# A valid username:
+# •	has length between 3 and 16 characters inclusive
+# •	can contain only letters, numbers, hyphens, and underscores
+# •	has no redundant symbols before, after, or in between
+
 import string
 
 
