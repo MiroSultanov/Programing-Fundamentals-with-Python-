089938386cb14f347589f4f7f8044913ae4dd128
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-24 22:55:05 +0300

    Update capture_the_numbers.py

--- regular_expressions/capture_the_numbers.py
+++ regular_expressions/capture_the_numbers.py
@@ -1,3 +1,5 @@
+# Write a program that receives strings on different lines and extracts only the numbers. Print all extracted numbers on a single line, separated by a single space.
+
 import re
 
 while True:
