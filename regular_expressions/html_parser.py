9280a94e8917a48066c7bb31ff351fc25eee5be0
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-24 23:06:32 +0300

    Update html_parser.py

--- regular_expressions/html_parser.py
+++ regular_expressions/html_parser.py
@@ -1,3 +1,16 @@
+# Write a program that extracts a title and all the content of a HTML file:
+# •	The title should be between the HTML tags <title> and <\title>. 
+# •	The content should be between the HTML tags <body> and <\body>.
+# There might be different HTML tags, which you should ignore: 
+# •	Each HTML tag is surrounded by the symbols "<" and ">". For example: <body>, <p>, <\html>
+# •	You also should ignore the HTML tag "\n"
+# Example: 
+# "<html>\n<head><title>News</title></head>\n<body><p><a href="https://softuni.bg">SoftUni</a>aims to provide free real-world practical\n training for young people 
+#  who want to turn into\n skillful Python software engineers.</p></body>\n</html>"
+# In this example the title is "News" and the content is "SoftUni aims to provide free real-world practical training for young people who want to turn into skillful 
+# Python software engineers."
+
+
 import re
 
 parser = re.compile(r'(?<=<title>)(.+)(?=</title>)|((?:(?<=\>)|(?<=\\n))[^\\<]+(?:(?=\<)|(?=\\n)))')
