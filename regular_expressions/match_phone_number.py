b748f9fc2f0410cc70a818c29d38d0f61ca3e50c
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-24 23:01:14 +0300

    Update match_phone_number.py

--- regular_expressions/match_phone_number.py
+++ regular_expressions/match_phone_number.py
@@ -1,10 +1,11 @@
-# import re
-#
-# phone_numbers = input()
-#
-# matches = re.findall(r"\+359 2 \d{3} \d{4}\b| \+359-2-\d{3}-\d{4}\b", phone_numbers)
-#
-# print(",".join(matches))
+# Write a regular expression to match a valid phone number from Sofia. After you find all valid phones, print them on the console, separated by a comma and a space ", ".
+# A valid number has the following characteristics:
+# •	It starts with "+359"
+# •	Then, it is followed by the area code (always 2)
+# •	After that, it is followed by a number:
+# o	The number consists of 7 digits (separated into two groups of 3 and 4 digits, respectively). 
+# •	The different parts are separated by either a space or a hyphen ('-').
+
 
 import re
 
